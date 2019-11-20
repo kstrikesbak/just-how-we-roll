a31e32f64d5e96ed869a62fb28665eacb8718608
Kejal Bhuva <Kejal.Bhuva@codeimmersives.com>
2019-11-19 21:31:08 -0500

    first 3 functions and event listeners done

--- main.js
+++ main.js
@@ -22,14 +22,55 @@ function getRandomNumber(max) {
 }
 
 
+
 /*******************
  * YOUR CODE BELOW *
  *******************/
+let newD6 = document.querySelector('#d6-roll');
+newD6.src = 'images/start/d6.png';
+let newD12 = document.querySelector('#d12-roll');
+newD12.src = 'images/start/d12.jpeg';
+let newD20 = document.querySelector('#d20-roll');
+newD20.src = 'images/start/d20.jpg';
+let newD6Double = document.querySelector('#double-d6-roll-1');
+newD6Double.src = 'images/start/d6.png';
+let newD6Double2 = document.querySelector('#double-d6-roll-2');
+newD6Double2.src = 'images/start/d6.png';
+
+function changeD6RollPic (){
+    let roll = getRandomNumber(6)
+    // const newD6 = document.querySelector('#d6-roll')
+    newD6.src = `./images/d6/${roll}.png`
+}
+// changeD6RollPic()
+
+function changeDoubleD6RollPics (){
+    let roll = getRandomNumber(6)
+    let rollAgain = getRandomNumber(6)
+    document.querySelector('#double-d6-roll-1').src=`./images/d6/${roll}.png`
+    document.querySelector('#double-d6-roll-2').src=`./images/d6/${rollAgain}.png`
+}
+
+function changeDoubleD12RollPic (){
+    let roll = getRandomNumber(12)
+    if (roll<=12) { 
+        document.querySelector('#d12-roll').src=`./images/numbers/${roll}.png`
+}
+}
 
 
 /*******************
  * EVENT LISTENERS *
  *******************/
+
+const singleD6 = document.querySelector('#d6-roll')
+singleD6.addEventListener('click',changeD6RollPic)
+const doubleD6First = document.querySelector('#double-d6-roll-1')
+doubleD6First.addEventListener('click',changeDoubleD6RollPics)
+const doubleD6Second = document.querySelector('#double-d6-roll-2')
+doubleD6Second.addEventListener('click',changeDoubleD6RollPics)
+const d12 = document.querySelector('#d12-roll')
+d12.addEventListener('click', changeDoubleD12RollPic)
 
 
 /****************
